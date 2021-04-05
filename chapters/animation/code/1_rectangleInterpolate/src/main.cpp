194ec10de06f0dc45f1c3cdc6969d32e3fc0de1d
Michael Ang <contactmang@gmail.com>
2021-04-05 13:33:33 +0400

    Remove dependency on GLUT

--- chapters/animation/code/1_rectangleInterpolate/src/main.cpp
+++ chapters/animation/code/1_rectangleInterpolate/src/main.cpp
@@ -1,12 +1,10 @@
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
 
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 600,600, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(600,600, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
